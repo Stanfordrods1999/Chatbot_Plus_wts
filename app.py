c24465fa779305f2c9511fec91929b0d8740de18
Stanny <127314109+Stanfordrods1999@users.noreply.github.com>
2023-07-11 13:41:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,31 +12,31 @@ import pandas as pd
 import requests
 from st_on_hover_tabs import on_hover_tabs
 
+st.markdown('<style>' + open('style.css').read() + '</style>', unsafe_allow_html=True)
+
 if "Functionality" not in st.session_state:
     st.cache_resource.clear()
     st.session_state.Functionality = False
     st.session_state.initialize = True
     st.session_state.dataset = []
-
-st.markdown('<style>' + open('style.css').read() + '</style>', unsafe_allow_html=True)
-with st.sidebar:
-    app_select = on_hover_tabs(tabName=['About App', 'ChatBot', 'Map'],
-                         iconName=['info', 'chat', 'map'],
-                         styles = {'navtab': {'background-color':'#111',
-                                              'color': '#818181',
-                                              'font-size': '18px',
-                                              'transition': '.3s',
-                                              'white-space': 'nowrap',
-                                              'text-transform': 'uppercase'},
-                                   'tabOptionsStyle': {':hover :hover': {'color': 'red',
-                                                                  'cursor': 'pointer'}},
-                                   'iconStyle':{'position':'fixed',
-                                                'left':'7.5px',
-                                                'text-align': 'left'},
-                                   'tabStyle' : {'list-style-type': 'none',
-                                                 'margin-bottom': '30px',
-                                                 'padding-left': '30px'}},
-                         key="1")
+    with st.sidebar:
+        app_select = on_hover_tabs(tabName=['About App', 'ChatBot', 'Map'],
+                             iconName=['info', 'chat', 'map'],
+                             styles = {'navtab': {'background-color':'#111',
+                                                  'color': '#818181',
+                                                  'font-size': '18px',
+                                                  'transition': '.3s',
+                                                  'white-space': 'nowrap',
+                                                  'text-transform': 'uppercase'},
+                                       'tabOptionsStyle': {':hover :hover': {'color': 'red',
+                                                                      'cursor': 'pointer'}},
+                                       'iconStyle':{'position':'fixed',
+                                                    'left':'7.5px',
+                                                    'text-align': 'left'},
+                                       'tabStyle' : {'list-style-type': 'none',
+                                                     'margin-bottom': '30px',
+                                                     'padding-left': '30px'}},
+                             key="1",default_choice=0)
 if app_select ==  "About App":
     if "paragraph" not in st.session_state:
         file_path = 'text.txt'
